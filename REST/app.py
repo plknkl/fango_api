5ded0272a55ce13cd317e58433d0e00a8d6bfdd6
Nikolai Polikurov <nikolai.polikurov@gnucoop.com>
2021-04-25 12:17:32 +0200

    evironment variable rest update

--- REST/app.py
+++ REST/app.py
@@ -9,7 +9,6 @@ from schemas.schemas import ShowSchema, EpisodeSchema
 from dotenv import load_dotenv
 load_dotenv()
 
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
